366e7a4022cb9c9dda6a4171e06a572e1c7ba991
Emil Olejnik <olejnike2000@gmail.com>
2021-08-16 16:47:11 +0200

    added function to set tip on btns

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,29 @@
 const peopleSection = document.querySelector('.app__people');
 const zeroError = peopleSection.querySelector('.zero-error');
+const billInput = document.querySelector('.app__bill-input');
+const peopleInput = peopleSection.querySelector('.app__people-input');
+const customValue = document.querySelector('.app__custom');
+const resetBtn = document.querySelector('.btn-reset');
+const tipValue = document.querySelector('.tip-value');
+const totalValue = document.querySelector('.total-value');
+const tipBtns = document.querySelectorAll('.app__tips-btn');
 
+let currentTip;
+
+const setBtnTip = e => {
+    if(e.target.textContent.length <= 2)
+    {
+        currentTip = e.target.textContent.slice(0, 1);
+    }
+    else
+    {
+        currentTip = e.target.textContent.slice(0, 2);
+    }
+    currentTip = (parseInt(currentTip)/100);
+}
+const setCustomTip = (params) => {
+    
+}
 const handleZeroError = () => {
     if(window.innerWidth < 992)
     {
@@ -15,4 +38,8 @@ const handleZeroError = () => {
         zeroError.classList.toggle('fail');
     }
 }
-handleZeroError()
+// handleZeroError()
+tipBtns.forEach(btn => {
+    btn.addEventListener('click', setTip);
+});
+customValue.addEventListener('input', setBtnTip);
